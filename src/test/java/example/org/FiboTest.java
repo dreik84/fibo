9d7225269459ec11aabf91ffa15142e65e7b84c6
dreik84 <dreik84@mail.ru>
2025-10-24 17:04:52 +0300

    Create FiboTest.java

---
+++ src/test/java/example/org/FiboTest.java
@@ -0,0 +1,25 @@
+package example.org;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FiboTest {
+
+    private static int numOfElement;
+
+    @BeforeAll
+    static void init() {
+        numOfElement = 1;
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181})
+    void findFibo(long expected) {
+        long actual = Fibo.findFibo(numOfElement++);
+
+        assertEquals(expected, actual);
+    }
+}
