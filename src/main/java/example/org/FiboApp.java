c879fac76d800c1746ac427831357d8033f4178c
dreik84 <dreik84@mail.ru>
2025-10-24 17:04:42 +0300

    Create FiboApp.java

---
+++ src/main/java/example/org/FiboApp.java
@@ -0,0 +1,16 @@
+package example.org;
+
+public class FiboApp {
+    public static void main(String[] args) {
+        int numOfElement = 50;
+
+        long start = System.currentTimeMillis();
+
+        long fibo = Fibo.findFibo(numOfElement);
+
+        long end = System.currentTimeMillis();
+
+        System.out.printf("Calculate the %dth element of the Fibonacci sequence: %d in %d milliseconds",
+                numOfElement, fibo, end - start);
+    }
+}
